1cad427f83e08adc98dd282dadff2c7092eb665b
Gerardo Orellana <jorellana2434@gmail.com>
2024-04-18 23:55:32 -0700

    updated page1

---
+++ your-world/src/Page1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Page from './Page';
+
+function Page1() {
+  return (
+    <Page>
+      <h1>Page 1</h1>
+      <p>This is page 1.</p>
+    </Page>
+  );
+}
+
+export default Page1;
