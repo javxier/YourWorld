bac17098ec547f291b2525c297381900180f5159
Gerardo Orellana <jorellana2434@gmail.com>
2024-04-18 23:38:17 -0700

    App.js updates

--- your-world/src/App.js
+++ your-world/src/App.js
@@ -4,15 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Small Website</h1>
-        <p>This is a simple website built with React.</p>
+        <h1>Welcome to the Beginning</h1>
+        <p>Reaction. Combination.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn It
         </a>
       </header>
     </div>
